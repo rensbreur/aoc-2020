fdc2f1d375893c8df2609e7772e462550f84735e
Rens Breur <rens.breur@lightspeedhq.com>
2020-12-01 14:56:29 +0100

    Added solutions for day 1

---
+++ Ex01.hs
@@ -0,0 +1,27 @@
+module Ex01 where
+
+import Data.Foldable (find)
+import Data.List (tails, sort)
+
+pairs :: [a] -> [[a]]
+pairs lst = [[x, y] | x:xs <- tails lst, y <- xs]
+
+triples :: [a] -> [[a]]
+triples lst = [x:pr | x:xs <- tails lst, pr <- pairs xs]
+
+condition :: [Int] -> Bool
+condition = (== 2020) . sum
+
+answer1 :: [Int] -> Maybe Int
+answer1 lst = product <$> find condition (pairs lst)
+
+answer2 :: [Int] -> Maybe Int
+answer2 lst = product <$> find condition (triples lst)
+
+ex01 :: IO ()
+ex01 = do
+  input <- readFile "input.txt"
+  let lst = read <$> lines input
+  let srtd = sort lst -- sorting improves speed
+  putStrLn $ "Answer 1: " ++ show (answer1 srtd)
+  putStrLn $ "Answer 2: " ++ show (answer2 srtd)
---
+++ Main.hs
@@ -0,0 +1,5 @@
+module Main where
+
+import Ex01
+
+main = ex01
